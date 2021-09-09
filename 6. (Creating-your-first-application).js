e246c08a0d391ae1d971c5e21737d5efe27efe8a
uchihaNayeem <uchiha.nayeem@gmail.com>
2021-09-10 02:09:44 +0600

    vid 6

---
+++ 6. (Creating-your-first-application).js
@@ -0,0 +1,7 @@
+let greet = 'hello';
+let sports = 'soccer';
+
+console.log(`${greet} ${sports}`);
+console.log('%s %s',greet,sports); // %s what is it??
+
+/* https://stackoverflow.com/questions/42406146/javascript-s-or-d-represents-string */
